31e959c0a29c7ad376caddecec762863090e207e
Shivam Singh <shivamssingh3098@gmail.com>
2025-05-05 16:42:53 +0530

    uploading app.config.js file

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // API_BASE_URL: "https://sra-government-project-thane-1.onrender.com", // <- change to your real base URL
-  API_BASE_URL: "http://192.168.1.155:8080",
+  API_BASE_URL: "https://srammrrts.in",
 };
 
 module.exports = CONFIG;
