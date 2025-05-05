a6b5948f3234e8339fb3905b2c6aa5f6dd559183
Shivam Singh <shivamssingh3098@gmail.com>
2025-05-05 17:14:57 +0530

    dekho

--- src/app.config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  // API_BASE_URL: "https://sra-government-project-thane-1.onrender.com", // <- change to your real base URL
-  API_BASE_URL: "https://srammrrts.in",
-};
-// now it should not be update on git... ho rha h update..  dekhte h ab ignore krta h ki nhi
-module.exports = CONFIG;
