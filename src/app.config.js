bc1aa61ae0bf5f8c3499fdcf172e2cda9e294eb6
Shivam Singh <shivamssingh3098@gmail.com>
2025-05-05 16:55:08 +0530

    should not be update

--- src/app.config.js
+++ src/app.config.js
@@ -2,5 +2,5 @@ const CONFIG = {
   // API_BASE_URL: "https://sra-government-project-thane-1.onrender.com", // <- change to your real base URL
   API_BASE_URL: "https://srammrrts.in",
 };
-
+// now it should not be update on git
 module.exports = CONFIG;
