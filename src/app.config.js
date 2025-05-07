2d5f208ff9e92b65f9fd4aca6106b9984a43995c
Shivam Singh <shivamssingh3098@gmail.com>
2025-05-07 17:25:19 +0530

    ignoring app.config.js file

--- src/app.config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  // API_BASE_URL: "https://sra-government-project-thane-1.onrender.com", // <- change to your real base URL
-  API_BASE_URL: "https://srammrrts.in",
-  // API_BASE_URL: "http://localhost:8080",
-  // checking
-};
-module.exports = CONFIG;
